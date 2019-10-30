7a54b825556dd47eddfaec7243188819e164c84d
Connor Eischen <cjeischen@dmacc.edu>
2019-10-30 04:52:23 -0500

    Test

---
+++ CIS174_TestCoreApp/Migrations/20191028045941_InitialSchema.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CIS174_TestCoreApp.Migrations
+{
+    public partial class InitialSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FamousPeoples",
+                columns: table => new
+                {
+                    firstName = table.Column<string>(nullable: true),
+                    FamousPeopleId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    lastName = table.Column<string>(nullable: true),
+                    birthDate = table.Column<string>(nullable: true),
+                    city = table.Column<string>(nullable: true),
+                    state = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FamousPeoples", x => x.FamousPeopleId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FamousPeoples");
+        }
+    }
+}
---
+++ CIS174_TestCoreApp/Migrations/20191030090113_Achievements.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CIS174_TestCoreApp.Migrations
+{
+    public partial class Achievements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "state",
+                table: "FamousPeoples",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastName",
+                table: "FamousPeoples",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstName",
+                table: "FamousPeoples",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "city",
+                table: "FamousPeoples",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "birthDate",
+                table: "FamousPeoples",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Achievements",
+                columns: table => new
+                {
+                    AchievementsId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FamousPeopleId = table.Column<int>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    doa = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Achievements", x => x.AchievementsId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Achievements");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "state",
+                table: "FamousPeoples",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "lastName",
+                table: "FamousPeoples",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "firstName",
+                table: "FamousPeoples",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "city",
+                table: "FamousPeoples",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "birthDate",
+                table: "FamousPeoples",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
